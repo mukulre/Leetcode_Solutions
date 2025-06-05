84ef3695dac7fec32f1a07dd9fa73b787ed6915a
Mukul Raj <142376335+mukulre@users.noreply.github.com>
2025-06-05 09:03:51 +0530

    Add files via upload

---
+++ Copythearray.java
@@ -0,0 +1,10 @@
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int []arr={23,14,2,54};
+	    int []newarr=arr;
+	    for(int val:newarr)System.out.println(val+"");
+	}
+}
