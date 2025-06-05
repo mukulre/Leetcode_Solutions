67cd91f68e893f3a859f795a7365e9ac816f682f
Mukul Raj <142376335+mukulre@users.noreply.github.com>
2025-06-05 13:52:23 +0530

    Add files via upload

---
+++ StringManipulation.java
@@ -0,0 +1,11 @@
+class StringManipulation {
+    public static void main(String[] args){
+        String name="Hello Java Learners";
+        String sample="Java is fun. Java is powerful";
+        
+        System.out.println("Length: "+name.length());
+        System.out.println("Char at 2: "+name.charAt(2));
+        System.out.println("Substring from 2: "+name.substring(2));
+        System.out.println("Substring 2 to 7: "+name.substring(2,7));
+    }
+}
