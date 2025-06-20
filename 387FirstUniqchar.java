eb114a80fbb300f2599b0885b0d9dcd51c685e93
Mukul Raj <142376335+mukulre@users.noreply.github.com>
2025-06-20 19:41:06 +0530

    Add files via upload

---
+++ 387FirstUniqchar.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];  // Only 26 lowercase English letters
+
+        // Count frequency of each character in the string
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Find the first character with frequency == 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1; // No unique character found
+    }
+}
+
